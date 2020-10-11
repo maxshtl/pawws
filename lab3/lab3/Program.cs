1bcb80eeb39a75975ed11d68a5ddaa05fe221fe8
nastyaR <maxshtl201@gmail.com>
2020-10-11 19:49:18 +0300

    almost forgot

---
+++ lab3/lab3/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*1) Определить класс, указанный в варианте, содержащий:
++		 Не менее трех конструкторов (с параметрами и без, а также с
+параметрами по умолчанию );
++	   	статический конструктор (конструктор типа);
++		определите закрытый конструктор; предложите варианты его вызова;
++		 поле - только для чтения (например, для каждого экземпляра сделайте
+поле только для чтения ID - равно некоторому уникальному номеру
+(хэшу) вычисляемому автоматически на основе инициализаторов
+объекта);
++		поле- константу;
++		свойства (get, set) – для всех полей класса (поля класса должны быть
+закрытыми); Для одного из свойств ограничьте доступ по set
++		в одном из методов класса для работы с аргументами используйте ref -
+и out-параметры.
++		создайте в классе статическое поле, хранящее количество созданных
+объектов (инкрементируется в конструкторе) и статический
+метод вывода информации о классе.
++		сделайте касс partial
++		переопределяете методы класса Object: Equals, для сравнения объектов,
+GetHashCode; для алгоритма вычисления хэша руководствуйтесь
+стандартными рекомендациями, ToString – вывода строки –
+информации об объекте.
+2)+		 Создайте несколько объектов вашего типа. Выполните вызов
+конструкторов, свойств, методов, сравнение объекты, проверьте тип
+созданного объекта и т.п.
+3)+		Создайте массив объектов вашего типа. И выполните задание,
+выделенное курсивом в таблице.
+4)+		 Создайте и выведите анонимный тип (по образцу вашего класса).
+
+
+	Создать класс Airline: Пункт назначения, Номер рейса,
+Тип самолета, Время вылета, Дни недели. Свойства и
+конструкторы должны обеспечивать проверку
+корректности.
+Создать массив объектов. Вывести:
+a)+		список рейсов для заданного пункта назначения;
+b)+		список рейсов для заданного дня недели;*/
+
+
+namespace lab3
+{
+	public partial class Airline
+	{
+		public string destination, airplane_type, day;
+		public int flight_number;
+		public double time;
+		public readonly int ID;
+		internal const int max_seats = 60;
+		public static int count;
+
+		static Airline()
+		{
+			count = 0;
+			Console.WriteLine("New post!");
+		}
+
+		private string Destination
+		{
+			get => destination;
+			set => destination = value;
+		}
+
+		internal string Airplane_type
+		{
+			get => airplane_type;
+			private set => airplane_type = value;
+		}
+
+		private string Day
+		{
+			get => day;
+			set => day = value;
+		}
+
+		private int Flight_number
+		{
+			get => flight_number;
+			set => flight_number = value;
+		}
+
+		private double Time
+		{
+			get => time;
+			set => time = value;
+		}
+
+		public Airline()
+		{
+			destination = "LA";
+			airplane_type = "business";
+			day = "tus";
+			flight_number = 1;
+			time = 10.15;
+			ID = flight_number.GetHashCode();
+			count++;
+		}
+
+		public Airline(string Destination, string Airplane_type, string Day, int Flight_number, double Time)
+		{
+			destination = Destination;
+			airplane_type = Airplane_type;
+			day = Day;
+			flight_number = Flight_number;
+			time = Time;
+			ID = flight_number.GetHashCode();
+			count++;
+		}
+
+		private Airline(string Day, double Time)
+		{
+			destination = "Tokyo";
+			airplane_type = "economy";
+			day = Day;
+			flight_number = 12;
+			time = Time;
+			ID = flight_number.GetHashCode();
+			count++;
+		}
+
+		public static Airline createAirlineToTokyo(string Day, double Time)
+		{
+			return new Airline(Day, Time);
+		}
+
+		public void Show()
+		{
+			Console.WriteLine("\nПункт назначения: " + destination + "\nНомер рейса: " + flight_number + "\nДень: " + day + "\nВремя: " + time.ToString() + "\nТип самолета: " + airplane_type);
+		}
+
+		public void ShowDay()
+		{
+			Console.WriteLine("\nПункт назначения: " + destination + "\nНомер рейса: " + flight_number + "\nВремя: " + time + "\nТип самолета: " + airplane_type);
+		}
+
+		public void ShowDest ()
+		{
+			Console.WriteLine("\nНомер рейса: " + flight_number + "\nДень: " + day + "\nВремя: " + time + "\nТип самолета: " + airplane_type);
+		}
+
+		public static void OutFlight(out int flight_number, ref double time)
+		{
+			flight_number = 0;
+			time = 0;
+		}
+
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Airline first = new Airline();
+			first.Show();
+
+			Airline second = Airline.createAirlineToTokyo("mon", 12.15);
+			second.Show();
+
+			Airline third = new Airline("NY", "economy", "sun", 2, 16.30);
+			third.Show();
+
+			Airline fourth = Airline.createAirlineToTokyo("sun", 16.30);
+			fourth.Show();
+
+			Airline fifth = new Airline("LA", "business", "thu", 3, 15.45);
+			fifth.Show();
+
+			Console.WriteLine(fifth.airplane_type == first.airplane_type);
+			Console.WriteLine(third.destination.GetType());
+
+			Airline[] List = { first, second, third, fourth, first };
+
+			string show_dest = "LA";
+			string show_day = "sun";
+			foreach (var x in List)
+			{
+				if (x.day == show_day)
+				{
+					Console.WriteLine("\nOn {0} we have: ", show_day);
+					x.ShowDay();
+				}
+			}
+
+			foreach (var x in List)
+			{
+				if (x.destination == show_dest)
+				{
+					Console.WriteLine("\nTo {0} flying: ", show_dest);
+					x.ShowDest();
+				}
+			}
+
+			var some_flight = new { destination = "LA", airplane_type = "business", day = "wed", flight_number = 5, time = 9 };
+			Console.WriteLine($"{some_flight}");
+
+			Console.ReadKey();
+		}
+	}
+}
---
+++ lab4/lab4/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* 
+	+1) Создать заданный в варианте класс. Определить в классе необходимые методы, конструкторы, индексаторы и заданные перегруженные
+	операции. Написать программу тестирования, в которой проверяется использование перегруженных операций.
+	+	Класс - стек Stack. Дополнительно перегрузить следующие операции: + - добавить элемент в стек; -- - извлечь элемент из
+	стека; true - проверка, пустой ли стек; > - копирование одного стека в другой с сортировкой в возрастающем порядке.
+
+	+?2) Добавьте в свой класс вложенный объект Owner, который содержит Id, имя и организацию создателя. Проинициализируйте его
+
+	+3) Добавьте в свой класс вложенный класс Date (дата создания). Проинициализируйте.
+
+	+4) Создайте статический класс StatisticOperation, содержащий 3 метода для работы с вашим классом.
+
+	+5) Добавьте к классу StatisticOperation методы расширения для типа string и вашего типа из задания№1.
+		Методы расширения:
+		+1) Подсчет количества предложений
+		+2) Определение среднего элемента стека
+ */
+namespace lab4
+{
+
+	class Stack
+	{
+		public int currentsize;
+		public int[] arrayData;
+		public int maxSize = 10;
+		public int value { get; set; }
+
+		private class Owner 
+		{
+			long id = 1235786554688;
+			string name = "Rykhlionak A.";
+			string organization = "Oooooo";
+		}
+
+		private class Date
+		{
+			double date = 11.10;
+		}
+
+		public Stack ()
+		{
+			currentsize = 0;
+			arrayData = new int[maxSize];
+		}
+
+		public static int operator +(int value, Stack myStack)
+		{
+			return myStack.arrayData[myStack.currentsize++] = value;
+		}
+
+		public static Stack operator --(Stack myStack)
+		{
+			myStack.currentsize--;
+			myStack.arrayData[myStack.currentsize] = 0;
+			return myStack;
+		}
+
+		public static bool operator true(Stack myStack)
+		{
+			return myStack.currentsize == 0;
+		}
+
+		public static bool operator false(Stack myStack)
+		{
+			return myStack.currentsize != 0;
+		}
+
+		public static Stack operator >(Stack myStack, Stack secondStack)
+		{
+			for (int i = 0; i < myStack.currentsize; i++)
+			{
+				secondStack.arrayData[secondStack.currentsize] = myStack.arrayData[i];
+				for (int j = secondStack.currentsize; j - 1 >= 0; j--)
+				{
+
+					if (secondStack.arrayData[j] < secondStack.arrayData[j - 1])
+					{
+						int temp = secondStack.arrayData[j];
+						secondStack.arrayData[j] = secondStack.arrayData[j - 1];
+						secondStack.arrayData[j - 1] = temp;
+					}
+				}
+				secondStack.currentsize++;
+			}
+			return secondStack;
+		}
+
+		public static Stack operator <(Stack myStack, Stack secondStack)
+		{
+			Console.WriteLine("someBODY ONCE TOLD ME-");
+			return secondStack;
+		}
+
+		public void Show()
+		{
+			for (int i = 0; i < this.currentsize; i++)
+			{
+				Console.Write(this.arrayData[i].ToString() + '\t');
+				Console.WriteLine(' ');
+			}
+		}
+	}
+
+	static class StatisticOperation
+	{
+		public static int Sum(Stack myStack)
+		{
+			return myStack.arrayData.Sum();
+		}
+
+		public static int Length(Stack myStack)
+		{
+			return myStack.arrayData.Length;
+		}
+
+		public static int Dif(Stack myStack)
+		{
+			return myStack.arrayData.Max() - myStack.arrayData.Min();
+		}
+
+		public static string Count(this string s)
+		{
+			// int count = 0; count.ToString()
+			string[] count_string = s.Split('.');
+			return count_string.Length.ToString();
+		}
+		public static double MidElem(this Stack myStack)
+		{
+			return myStack.arrayData.Sum()/myStack.currentsize;
+		}
+	}
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Stack myStack = new Stack();
+			Console.WriteLine("Добавление:");
+			Console.WriteLine(4 + myStack);
+			Console.WriteLine(7 + myStack);
+			Console.WriteLine(0 + myStack);
+			Console.WriteLine(9 + myStack);
+			Console.WriteLine(15 + myStack);
+			myStack.Show();
+
+			Console.WriteLine("Дикремент и тру:");
+			myStack--;
+			myStack.Show();
+			Console.WriteLine(myStack);//true??????
+
+			Console.WriteLine("Второй стек:");
+			Stack secondStack = new Stack();
+			Console.WriteLine(myStack > secondStack);
+			secondStack.Show();
+
+			Console.WriteLine("Методы (сумма, длина, max-min) и два расширенных (строки и среднее):");
+			Console.WriteLine(StatisticOperation.Sum(myStack));
+			Console.WriteLine(StatisticOperation.Length(myStack)); 
+			Console.WriteLine(StatisticOperation.Dif(myStack));
+			Console.WriteLine(StatisticOperation.Count("I.DONT.WANNA.BE.ME"));
+			Console.WriteLine(StatisticOperation.MidElem(myStack));
+			Console.ReadKey();
+		}
+	}
+}
